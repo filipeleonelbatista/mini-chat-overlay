d7b3c1d1d48d777c511f2bbdf3d99318d2d00440
Filipe Batista <filipe.x@live.com>
2024-07-26 11:07:00 -0300

    update size of window

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,8 +6,10 @@ import icon from '../../resources/icon.png?asset'
 function createWindow(): void {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 900,
-    height: 670,
+    width: 300,
+    minWidth: 300,
+    height: 500,
+    minHeight: 500,
     show: false,
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
